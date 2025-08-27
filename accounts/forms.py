df733c7d02e80c0f3ee4e78271f2f0bebd4c4edb
NevVaek <preyhunter1541@protonmail.com>
2025-08-27 22:06:18 +0900

    password change and reset

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,10 +1,33 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
+class CustomLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({
+            'placeholder': 'Username'
+        })
+        self.fields['password'].widget.attrs.update({
+            'placeholder': 'Password'
+        })
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ("username", "email", "age",)
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'Username'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Email address'}),
+            "age" : forms.NumberInput(attrs={'placeholder': 'Age'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Confirm password'
+
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 # Create your views here.
+
+from .forms import CustomUserCreationForm, CustomLoginForm
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+
+
+class LoginView(CreateView):        #PROTOTYPE NOT USED
+    form_class = CustomLoginForm
+    success_url = reverse_lazy('home')
+    template_name = 'registration/login.html'
+
